512eb638b6b5dd9ced2444746f30bf83acc5df54
DevSata <DevAppStudio@outlook.fr>
2024-10-20 18:23:28 +0100

    v31

--- components/othersSections/Forther.tsx
+++ components/othersSections/Forther.tsx
@@ -1,10 +1,11 @@
 import { LuRedo } from "react-icons/lu";
 import { IoMdCloudUpload } from "react-icons/io";
 import { GoCheckCircle } from "react-icons/go";
-
+import { FaClock } from "react-icons/fa6";
+import { FaTiktok } from "react-icons/fa";
 const TheForthSection = () => {
   return (
-    <section className="flex justify-center items-center w-full bg-base-200/50 text-base-content py-32 md:py-32 bg-slate-900">
+    <section className="flex justify-center items-center w-full bg-base-200/50 text-base-content py-24 md:py-24 bg-slate-900">
       <div className="flex flex-col max-w-[82rem] gap-16 md:gap-20 px-4">
         <h2 className="max-w-3xl font-black text-6xl md:text-6xl tracking-tight">
           Scroll less <br /> and{" "}
@@ -15,6 +16,7 @@ const TheForthSection = () => {
         <div className="flex flex-col w-full h-fit gap-4 lg:gap-10 text-text-default">
           <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:gap-10">
             {/* //----------------// */}
+
             <div className="bg-green-700 text-primary-content rounded-3xl flex flex-col gap-6 w-full h-[32rem] lg:h-[32rem] w-[24rem] pt-6 overflow-hidden group">
               <div className="px-4 space-y-2 ">
                 <h3 className="font-semibold sm:text-xl text-4xl tracking-tight">
@@ -42,9 +44,9 @@ const TheForthSection = () => {
             </div>
 
             {/*  //---------------//- */}
-            <div className="md:col-span-2 bg-base-300 text-base-content rounded-3xl flex flex-col gap-6 w-full h-[28rem] lg:h-[32rem] pt-6 overflow-hidden group">
+            <div className="bg-slate-700 md:col-span-2 bg-base-300 text-base-content rounded-3xl flex flex-col gap-6 w-full h-[32rem] lg:h-[32rem] pt-6 overflow-hidden group">
               <div className="px-6 space-y-2">
-                <h3 className="font-bold text-xl lg:text-3xl tracking-tight">
+                <h3 className="font-bold text-xl text-white lg:text-3xl tracking-tight">
                   Cure your chaotic mind
                 </h3>
                 <p className="opacity-80">
@@ -53,61 +55,135 @@ const TheForthSection = () => {
                 </p>
               </div>
               <div className="px-6 max-w-[600px] flex flex-col gap-4 overflow-hidden">
-                <div className="bg-base-100 p-4 rounded-box">
-                  <h3 className="font-semibold mb-2">Scheduled Posts</h3>
-                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-                    <div className="card bg-base-150">
-                      <div className="card-body p-4">
-                        <div className="flex justify-between items-start mb-2">
-                          <h3 className="card-title text-sm">
-                            <GoCheckCircle className="mr-2" />
-                            Tomorrow
-                          </h3>
-                          <div className="text-sm">
-                            <GoCheckCircle className="inline mr-1" />
-                            9:00 AM
-                          </div>
-                        </div>
-                        <p className="text-sm mb-4">Easy 5-min Workout</p>
-                        <div className="flex justify-between items-center">
-                          <div className="flex space-x-2">
-                            <div className="w-8 h-8 rounded-full bg-base-300 flex items-center justify-center">
-                              <GoCheckCircle className="inline mr-1" />
+                <div className="bg-slate-400 p-4 rounded-3xl">
+                  <h3 className="font-semibold mb-2 text-black">
+                    Scheduled Posts
+                  </h3>
+                  <div className="grid grid-cols-2 gap-4 ">
+                    {/* First Post */}
+                    <div className=" shadow-md overflow-hidden flex items-stretch h-[10rem] bg-green-100 rounded-3xl">
+                      <div className="card bg-base-800 rounded-box">
+                        <div className="card-body p-4">
+                          <div className="flex grid grid-cols-2 gap-10  mb-2">
+                            <h3 className="card-title text-sm flex items-center text-black ">
+                              <FaClock className="mr-1 text-black" />
+                              <h3 className="font-semibold">Tomorrow</h3>
+                            </h3>
+                            <div className="text-sm flex items-center">
+                              <FaClock className="mr-1 text-black" />
+                              <h2 className="text-black">9:00 AM</h2>
                             </div>
-                            <div className="text-sm">60 minutes</div>
                           </div>
-                          <div className="flex">
-                            <button className="btn btn-error btn-sm">
-                              Cancel
-                            </button>
+                          <p className="text-sm mb-4 text-black">
+                            Easy 5-min Workout
+                          </p>
+                          <div className="flex justify-between items-center">
+                            <div className="flex space-x-2 pt-3">
+                              <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                                <FaTiktok />
+                              </div>
+                              <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                                <FaTiktok className="rounded-full" />{" "}
+                              </div>
+                            </div>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <div className="card bg-base-150">
+
+                    {/* Second Post */}
+                    <div className=" shadow-md overflow-hidden flex items-stretch h-[10rem] bg-green-100 rounded-3xl">
                       <div className="card-body p-4">
-                        <div className="flex justify-between items-start mb-2">
-                          <h3 className="card-title text-sm">
-                            <GoCheckCircle className="mr-2" />
-                            Later
+                        <div className="flex grid grid-cols-2 gap-10 mb-2">
+                          <h3 className="card-title text-sm flex items-center text-black">
+                            <FaClock className="mr-1 " />{" "}
+                            <h3 className="font-semibold">Friday</h3>
                           </h3>
-                          <div className="text-sm">
-                            <GoCheckCircle className="inline mr-1" />
-                            12:00 PM
+                          <div className="text-sm flex items-center text-black">
+                            <FaClock className="mr-1" /> <h2>2:00 PM</h2>
                           </div>
                         </div>
-                        <p className="text-sm mb-4">Evening Meditation</p>
+                        <p className="text-sm mb-4 text-black">
+                          Cooking Hacks #23
+                        </p>
                         <div className="flex justify-between items-center">
-                          <div className="flex space-x-2">
-                            <div className="w-8 h-8 rounded-full bg-base-300 flex items-center justify-center">
-                              <GoCheckCircle />
+                          <div className="flex space-x-2 pt-3">
+                            <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                              <FaTiktok />
                             </div>
-                            <div className="text-sm">45 minutes</div>
+                            <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                              <FaTiktok />
+                            </div>
+                            <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                              <FaTiktok />
+                            </div>
                           </div>
-                          <div className="flex">
-                            <button className="btn btn-error btn-sm">
-                              Cancel
-                            </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-slate-400 p-4 rounded-3xl">
+                  <h3 className="font-semibold mb-2 text-black">Live Posts</h3>
+                  <div className="grid grid-cols-2 gap-4 ">
+                    {/* First Post */}
+                    <div className=" shadow-md overflow-hidden flex items-stretch h-[10rem] bg-green-100 rounded-3xl">
+                      <div className="card bg-base-800 rounded-box">
+                        <div className="card-body p-4">
+                          <div className="flex grid grid-cols-2 gap-10  mb-2">
+                            <h3 className="card-title text-sm flex items-center text-black ">
+                              <FaClock className="mr-1 text-black" />
+                              <h3 className="font-semibold">2h ago</h3>
+                            </h3>
+                            <div className="text-sm flex items-center">
+                              <div className="btn text-xs bg-green-700 px-3 py-1.5 rounded-full text-white ">
+                                View Post
+                              </div>
+                            </div>
+                          </div>
+                          <p className="text-sm mb-4 text-black">
+                            Productivity Tips you need to know
+                          </p>
+                          <div className="flex justify-between items-center">
+                            <div className="flex space-x-2 pt-3">
+                              <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                                <FaTiktok />
+                              </div>
+                              <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                                <FaTiktok className="rounded-full" />{" "}
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    {/* Second Post */}
+                    <div className=" shadow-md overflow-hidden flex items-stretch h-[10rem] bg-green-100 rounded-3xl">
+                      <div className="card-body p-4">
+                        <div className="flex grid grid-cols-2 gap-10 mb-2">
+                          <h3 className="card-title text-sm flex items-center text-black">
+                            <FaClock className="mr-1 " />{" "}
+                            <h3 className="font-semibold">Friday</h3>
+                          </h3>
+                          <div className="btn text-xs bg-green-700 px-3 py-1.5 rounded-full text-white ">
+                            View Post
+                          </div>
+                        </div>
+                        <p className="text-sm mb-4 text-black">
+                          The Iphone 16... sucks...
+                        </p>
+                        <div className="flex justify-between items-center">
+                          <div className="flex space-x-2 pt-3">
+                            <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                              <FaTiktok />
+                            </div>
+                            <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                              <FaTiktok />
+                            </div>
+                            <div className="w-10 h-10 rounded-full bg-slate-500 flex items-center justify-center shadow-lg">
+                              <FaTiktok />
+                            </div>
                           </div>
                         </div>
                       </div>
