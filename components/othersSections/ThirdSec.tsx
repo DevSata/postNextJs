9712b18e6c3633405e9bdd695a1de9672ee1af1a
DevSata <DevAppStudio@outlook.fr>
2024-10-22 16:59:43 +0100

    v60

--- components/othersSections/ThirdSec.tsx
+++ components/othersSections/ThirdSec.tsx
@@ -1,6 +1,7 @@
 import { LuRedo } from "react-icons/lu";
 import { GoCheckCircle } from "react-icons/go";
 import React, { useState } from "react";
+
 const TheThirdSection = () => {
   const [isVisible01, setIsVisible01] = useState<boolean>(true);
   const [isVisible02, setIsVisible02] = useState<boolean>(false);
@@ -23,6 +24,23 @@ const TheThirdSection = () => {
     setIsVisible04((prev) => !prev);
   };
 
+  //---------
+  const [visibleStates, setVisibleStates] = useState({
+    isVisible01: true,
+    isVisible02: false,
+    isVisible03: false,
+    isVisible04: false,
+  });
+
+  const toggleVisibility = (key: string) => {
+    setVisibleStates((prev) => ({
+      ...prev,
+      [key]: !prev[key as keyof typeof prev],
+    }));
+  };
+
+  //---------------
+
   return (
     <section
       className="py-10 md:py-32 space-y-24 md:space-y-32 max-w-7xl mx-auto bg-base-100 xl:px-52 lg:px-52 md:px-32 sm:px-16"
@@ -39,6 +57,13 @@ const TheThirdSection = () => {
         <div className="flex flex-col md:flex-row gap-12 md:gap-24">
           <div className="grid sm:grid-cols-1 sm:gap-8 md:grid-cols-1 md:gap-8 items-stretch  grid-cols-2 gap-20">
             <ul className="w-full">
+              <ModelCards
+                title="questions 2"
+                description="The sky whispered secrets as the wind gently danced, A melody played on golden fields of endless light, And time stood still in the heart of a dream."
+                toSee={isVisible01}
+                onSomeEvent={toggleVisibility01}
+              />
+
               <li>
                 <button
                   className="relative flex gap-2 items-center w-full py-5 text-base font-medium text-left md:text-lg"
@@ -211,3 +236,41 @@ const TheThirdSection = () => {
 };
 
 export default TheThirdSection;
+
+// aadded models here
+
+const ModelCards = ({ title, description, toSee, onSomeEvent }: Props) => {
+  return (
+    <li>
+      <button
+        className="relative flex gap-2 items-center w-full py-5 text-base font-medium text-left md:text-lg"
+        aria-expanded={toSee}
+        onClick={() => toggleVisibility($toSee)}
+      >
+        <span
+          className={`duration-100 ${
+            toSee ? "text-green-500" : "text-primary"
+          }`}
+        >
+          <GoCheckCircle className="w-5 h-5" />
+        </span>
+        <span className="flex-1 text-base-content text-primary font-semibold">
+          <h3
+            className={`inline ${
+              toSee
+                ? "text-green-500"
+                : "text-base-content text-primary font-semibold"
+            }`}
+          >
+            {title}
+          </h3>
+        </span>
+      </button>
+      {toSee && (
+        <div className="transition-all duration-300 ease-in-out text-base-content-secondary overflow-hidden ">
+          <div className="pb-5 leading-relaxed ml-5">{description}</div>
+        </div>
+      )}
+    </li>
+  );
+};
